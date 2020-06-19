6008b891d336aa85cea43060f87d2930774a357c
Nathan <nathan@bluemesaengineering.com>
2020-06-19 14:26:27 -0400

    feat:Completed tutorial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as BellIcon } from './icons/bell.svg';
 import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
 import { ReactComponent as CaretIcon } from './icons/caret.svg';
@@ -8,13 +8,116 @@ import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
 import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
 import { ReactComponent as BoltIcon } from './icons/bolt.svg';
 
+import { CSSTransition } from 'react-transition-group';
+
 function App() {
   return (
     <Navbar>
       <NavItem icon = { <PlusIcon /> } />
-      <NavItem icon = { <BellIcon /> }/>
+      <NavItem icon = { <BellIcon /> } />
       <NavItem icon = { <MessengerIcon /> } />
+
+      <NavItem icon = { <CaretIcon /> }>
+
+        <DropdownMenu/>
+
+      </NavItem>
+
     </Navbar>
+  );
+}
+
+function DropdownMenu() {
+
+  const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null)
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
+  function DropdownItem(props) {
+    return(
+      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+
+        <span className="icon-button">{ props.leftIcon }</span>
+        { props.children }
+
+        <span className="icon-right">{ props.rightIcon }</span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown" style={{ height: menuHeight }}>
+      <CSSTransition 
+        in={activeMenu === 'main'} 
+        unmountOnExit 
+        timeout={500}
+        classNames='menu-primary'
+        onEnter={calcHeight}
+        >
+          <div className='menu'>
+            <DropdownItem>My Profile</DropdownItem>
+            <DropdownItem
+              leftIcon={<CogIcon />}
+              rightIcon={<ChevronIcon />}
+              goToMenu="settings"
+              >                
+                Settings
+            </DropdownItem>
+          </div>
+      </CSSTransition>
+
+      <CSSTransition 
+        in={activeMenu === 'settings'} 
+        unmountOnExit 
+        timeout={500}
+        classNames='menu-secondary'
+        onEnter={calcHeight}
+        >
+          <div className='menu'>
+            <DropdownItem leftIcon={<ArrowIcon />} goToMenu='main' />
+            <DropdownItem
+              leftIcon={<CogIcon />}
+              rightIcon={<ChevronIcon />}>
+                Settings
+            </DropdownItem>
+            <DropdownItem
+              leftIcon={<CogIcon />}
+              rightIcon={<ChevronIcon />}>
+                Settings
+            </DropdownItem>
+            <DropdownItem
+              leftIcon={<CogIcon />}
+              rightIcon={<ChevronIcon />}>
+                Settings
+            </DropdownItem>
+            <DropdownItem
+              leftIcon={<CogIcon />}
+              rightIcon={<ChevronIcon />}>
+                Settings
+            </DropdownItem>
+            <DropdownItem
+              leftIcon={<CogIcon />}
+              rightIcon={<ChevronIcon />}>
+                Settings
+            </DropdownItem>
+            <DropdownItem
+              leftIcon={<CogIcon />}
+              rightIcon={<ChevronIcon />}>
+                Settings
+            </DropdownItem>
+            <DropdownItem
+              leftIcon={<CogIcon />}
+              rightIcon={<ChevronIcon />}>
+                Settings
+            </DropdownItem>
+          </div>
+      </CSSTransition>
+
+    </div>
   );
 }
 
@@ -27,11 +130,16 @@ function Navbar(props) {
 }
 
 function NavItem(props) {
+
+  const [open, setOpen] = useState(false);
   return(
     <li>
-      <a href="#" className="icon-button" >
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)} >
         { props.icon }
       </a>
+
+      {open && props.children}
+
     </li>
   );
 }
