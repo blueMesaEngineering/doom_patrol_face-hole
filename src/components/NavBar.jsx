70ae2f6e491bed75c221443b562bdaa4ea4e4bdf
Nathan <nathan@bluemesaengineering.com>
2020-06-30 11:35:01 -0400

    feat:New home background and reworked NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,47 +1,24 @@
 import React, { useState } from 'react';
-import { ReactComponent as BellIcon } from './icons/bell.svg';
-import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
-import { ReactComponent as CaretIcon } from './icons/caret.svg';
-import { ReactComponent as PlusIcon } from './icons/plus.svg';
-import { ReactComponent as CogIcon } from './icons/cog.svg';
-import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
-import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
-import { ReactComponent as BoltIcon } from './icons/bolt.svg';
+// import { ReactComponent as BellIcon } from './icons/bell.svg';
+// import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+// import { ReactComponent as CaretIcon } from './icons/caret.svg';
+// import { ReactComponent as PlusIcon } from './icons/plus.svg';
+// import { ReactComponent as CogIcon } from './icons/cog.svg';
+// import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+// import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+// import { ReactComponent as BoltIcon } from './icons/bolt.svg';
 import { CSSTransition } from 'react-transition-group';
 
-
 import { Router } from '@reach/router'
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import Ndguthrie from './pages/about/Ndguthrie'
-import Shallphd from './pages/about/Shallphd'
-import Distralite from './pages/projects/Distralite'
-import Minotaurshoof from './pages/projects/Minotaurshoof'
+import Home from '../pages/Home'
+import Contact from '../pages/Contact'
+import Ndguthrie from '../pages/about/Ndguthrie'
+import Shallphd from '../pages/about/Shallphd'
+import Distralite from '../pages/projects/Distralite'
+import Minotaurshoof from '../pages/projects/Minotaurshoof'
 
 
-function App() {
-  const navLinks = [
-    {
-      text: 'Home',
-      path: '/',
-      // icon: 'ion-ios-home',
-    },
-    {
-      text: 'Contact',
-      path: '/Contact',
-      // icon: 'ion-ios-megaphone',
-    },
-    {
-      text: 'About',
-      path: '/about',
-      // icon: 'ion-ios-business',
-    },
-    {
-      text: 'Portfolio',
-      path: '/portfolio',
-      // icon: 'ion-ios-briefcase',
-    }
-  ]
+function NavBar({ navLinks }) {
 
   return (
     <div>
@@ -190,4 +167,4 @@ function NavItem(props) {
   );
 }
 
-export default NavbarDropdownMenu;
+export default NavBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import gearHousing from "../assets/Images/Backgrounds/gearHousingBackground.png";
+import background from '../assets/Images/Backgrounds/blueEngineeringBackground.jpg'
 
 const styles = {
-  // backgroundStyle: {
-  //   width: "100%",
-  //   height: "50%",
-  //   backgroundImage: `url(${Background})`
-  // },
+  backgroundStyle: {
+    width: "100%",
+    height: "50%",
+    backgroundImage: `url(${background})`
+  },
   gearHousingStyle: {
     width: "50%",
     backgroundImage: `url(${gearHousing})`
@@ -28,23 +29,26 @@ class Home extends React.Component {
         <div> 
           <center>
             <div style={{ 
-                    backgroundImage: `url(${gearHousing})`, 
-                    backgroundPositionY: "-100px",
+                    backgroundImage: `url(${background})`, 
+                    // backgroundPositionY: "-100px",
                     backgroundSize: "cover",
-                    height: "80vh",
-                    width: "60vw"
+                    height: "60vh",
+                    width: "70vw"
                 }}
             >
                 <h2 className="content" style={{ paddingTop: "40px", fontSize: 30 }}>Design. Build. Create. Learn.</h2>
-                <p>Blue Mesa Engineering was born out of a brainstorming effort to solve a real world design engineering problem - a classic case of "How do we build a better mousetrap?"  Since then, having developed a tactical product, it has been growing organcially in the field of software engineering, and is always interested to learn about new problems to solve.</p>
-                <p>Have an idea?</p>
-                <p>Want to discuss?</p>
-                <p>
-                    We are currently active in the Software Engineering,
-                    Mechanical Design Engineering, and Biomedical
-                    Engineering spaces.
-                </p>
-                <p>Have a look at our projects for more information.</p>
+                <h3 style={{ paddingRight: "100px",
+                              paddingLeft: "100px"}}>
+                  <p>Blue Mesa Engineering was born out of a brainstorming effort to solve a real world design engineering problem - a classic case of "How do we build a better mousetrap?"  Since then, having developed a tactical product, it has been growing organcially in the field of software engineering, and is always interested to learn about new problems to solve.</p>
+                  <p>Have an idea?</p>
+                  <p>Want to discuss?</p>
+                  <p>
+                      We are currently active in the Software Engineering,
+                      Mechanical Design Engineering, and Biomedical
+                      Engineering spaces.
+                  </p>
+                  <p>Have a look at our projects for more information.</p>
+                </h3>
             </div>
           </center>
         </div>
