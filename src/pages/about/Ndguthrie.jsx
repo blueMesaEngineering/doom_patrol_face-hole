6254caaf7d5d20813e62bfaa87d5cfaf3b218b61
Nathan <nathan@bluemesaengineering.com>
2020-07-02 01:26:15 -0400

    feat:Updated profile with mobile first styles. WIP

--- src/pages/about/Ndguthrie.jsx
+++ src/pages/about/Ndguthrie.jsx
@@ -9,53 +9,29 @@ class Ndguthrie extends React.Component {
 
   render() {
     return (
-      <div id="parent">
-        <center>
-          <div style={{ padding: "0px" }}>
-            <center>
-              <h2 className="content" style={{ paddingTop: "40px", fontSize: 30 }}>Collaborators</h2>
-            </center>
+      <center>
+        <ContentWrap>
+          <div className="boxWrap">
+            <img  className="img"
+                  src={ Nathan }
+                  style={{ width: "30vh",
+                            paddingTop: "5%"
+                        }}/>
+            <div className="cont">
+              <p>
+                I am a Software Engineer open to new opportunities. I am currently re-exploring Python after dabbling in it several years ago in classes. I'm also dabbling with some Raspberry Pi and other embedded projects using the Qt5 framework. My background consists primarily of C++ as well as Ruby, Rails, Visual Basic, Python, .NET, and others.
+              </p>
+              <p>
+                I also work on side projects such as the Distralite, a collaborative effort with Dr. Hall and local law enforcement to build a "less lethal" distraction device.
+              </p>
+              <p>
+                In July 2018, I began Flight Training to work towards my Private Pilot's license, and the experience has been quite enjoyable.
+              </p>
+              <p>M.S. Mechanical Engineering, Purdue University, 2013</p>
+            </div>
           </div>
-          <div>
-            <table className="standardTable">
-              <tr>
-                <th>
-                  <img 
-                      className="img"
-                      src={Nathan}
-                      alt={"ND Guthrie"}
-                      style={{ width: "30vh",
-                      paddingLeft: "5%",
-                      paddingRight: "5%" }} />
-
-                  <p>Nathan D. Guthrie</p>
-                </th>
-                <th>
-                  <p>
-                    I am a Software Engineer open to new opportunities. 
-                    I am currently re-exploring Python after dabbling in it several 
-                    years ago in classes. I'm also dabbling with some Raspberry Pi 
-                    and other embedded projects using the Qt5 framework. 
-                    My background consists primarily of C++ as well as Ruby, Rails, 
-                    Visual Basic, Python, .NET, and others.
-                  </p>
-                  <p>
-                    I also work on side projects such as the Distralite, a
-                    collaborative effort with Dr. Hall and local law enforcement
-                    to build a "less lethal" distraction device.
-                  </p>
-                  <p>
-                    In July 2018, I began Flight Training to work towards my
-                    Private Pilot's license, and the experience has been quite
-                    enjoyable.
-                  </p>
-                  <p>M.S. Mechanical Engineering, Purdue University, 2013</p>
-                </th>
-              </tr>
-            </table>
-          </div>
-        </center>
-      </div>
+        </ContentWrap>
+      </center>
     );
   }
 }
