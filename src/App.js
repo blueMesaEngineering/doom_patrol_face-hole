862bd969a0b635ae435e1df922da89534c2bb040
Nathan <nathan@bluemesaengineering.com>
2020-07-07 21:40:38 -0400

    feat:Moved router code out of NavBar to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React, { useState } from 'react';
+import { Router } from '@reach/router'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/about/About'
+import Ndguthrie from './pages/about/Ndguthrie'
+import Shallphd from './pages/about/Shallphd'
+import Distralite from './pages/projects/Distralite'
+import Minotaurshoof from './pages/projects/Minotaurshoof'
+
 import NavBar from './components/NavBar';
 
 function App() {
@@ -28,6 +37,15 @@ function App() {
   return (
     <div>
       <NavBar navLinks={ navLinks } />
+      <Router>
+        < Contact path = "/contact" />
+        < Home path = "/" />
+        < About path = "/about" />
+        < Ndguthrie path = "/ndguthrie" />
+        < Shallphd path = "/shallphd" />
+        < Distralite path = "/distralite" />
+        < Minotaurshoof path = "/minotaurshoof" />
+      </Router>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,5 @@
 import React, { useState } from 'react';
-// import { ReactComponent as BellIcon } from './icons/bell.svg';
-// import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
-// import { ReactComponent as CaretIcon } from './icons/caret.svg';
-// import { ReactComponent as PlusIcon } from './icons/plus.svg';
-// import { ReactComponent as CogIcon } from './icons/cog.svg';
-// import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
-// import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
-// import { ReactComponent as BoltIcon } from './icons/bolt.svg';
 import { CSSTransition } from 'react-transition-group';
-
-import { Router } from '@reach/router'
-import Home from '../pages/Home'
-import Contact from '../pages/Contact'
-import About from '../pages/about/About'
-import Ndguthrie from '../pages/about/Ndguthrie'
-import Shallphd from '../pages/about/Shallphd'
-import Distralite from '../pages/projects/Distralite'
-import Minotaurshoof from '../pages/projects/Minotaurshoof'
 
 
 function NavBar({ navLinks }) {
@@ -35,17 +18,6 @@ function NavBar({ navLinks }) {
         </NavItem>
         <NavItem icon = { <h2><a href="/contact">Contact</a></h2> }/>
       </NavbarDropdownMenu>
-
-      <Router>
-      < Contact path = "/contact" />
-      {/* < Portfolio path = "/portfolio" /> */}
-      < Home path = "/" />
-      < About path = "/about" />
-      < Ndguthrie path = "/ndguthrie" />
-      < Shallphd path = "/shallphd" />
-      < Distralite path = "/distralite" />
-      < Minotaurshoof path = "/minotaurshoof" />
-      </Router>
     </div>
   );
 }
@@ -63,11 +35,7 @@ function AboutDropdownMenu() {
   function DropdownItem(props) {
     return(
       <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
-
-        {/* <span className="icon-button">{ props.leftIcon }</span> */}
         { props.children }
-
-        {/* <span className="icon-right">{ props.rightIcon }</span> */}
       </a>
     );
   }
@@ -116,11 +84,7 @@ function ProjectsDropdownMenu() {
   function DropdownItem(props) {
     return(
       <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
-
-        {/* <span className="icon-button">{ props.leftIcon }</span> */}
         { props.children }
-
-        {/* <span className="icon-right">{ props.rightIcon }</span> */}
       </a>
     );
   }
